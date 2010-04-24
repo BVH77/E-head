5f7e0ceac3822bc9c5a84a798ac30c5fa2e5ed44
bvh <bvh@2e978b77-0c5f-4b2b-b7d0-db7ae35eb1e0>
2010-04-24 08:28:35 +0000

    git-svn-id: file:///var/svn-repos/e-head/trunk@15 2e978b77-0c5f-4b2b-b7d0-db7ae35eb1e0

--- js/Library/OSDN/Layout.js
+++ js/Library/OSDN/Layout.js
@@ -47,123 +47,20 @@ OSDN.Layout.Workspace = Ext.extend(Ext.Viewport, {
     
     initComponent: function() {
         
-        this.menuToolbar = new Ext.Toolbar({
-            region: 'north',
+		this.menuToolbar = new Ext.Toolbar({
+        	region: 'north',
             height: 30,
-            items: [{
-                xtype: 'box',
-                autoEl: {
-                    tag: 'div',
-                    style: 'cursor: pointer;',
-                    qtip: 'e-head.ru',
-                    cls: 'e-head-logo'
-                },
-                listeners: {
-                    render: function(box) {
-                        box.el.on('click', function() {
-                            window.open('http://e-head.ru/');
-                        })
-                    }
-                }
-            }, ' ', '-', {
-                text: 'Архив заказов',
-                iconCls: 'archive-icon',
-                hidden: !acl.isView('archive'),
-                handler: function() {
-                    OSDN.System.Layout.getTabPanel().add({
-                        iconCls: 'archive-icon',
-                        xtype: 'PMS.Orders.Archive',
-                        id: 'PMS.Orders.Archive'
-                    });
-                }
-            }, {
-                text: 'Контрагенты',
-                iconCls: 'customers-icon',
-                menu: [{
-                    text: 'Заказчики',
-                    iconCls: 'customers-icon',
-                    hidden: !acl.isView('customers'),
-                    handler: function() {
-                        OSDN.System.Layout.getTabPanel().add({
-                            title: 'Заказчики',
-                            iconCls: 'customers-icon',
-                            entity: 'customers',
-                            xtype: 'PMS.ContragentsListAbstract',
-                            id: 'PMS.Customers.List'
-                        });
-                    }
-                }, {
-                    text: 'Поставщики',
-                    iconCls: 'suppliers-icon',
-                    hidden: !acl.isView('suppliers'),
-                    handler: function() {
-                        OSDN.System.Layout.getTabPanel().add({
-                            title: 'Поставщики',
-                            iconCls: 'suppliers-icon',
-                            entity: 'suppliers',
-                            xtype: 'PMS.ContragentsListAbstract',
-                            id: 'PMS.Suppliers.List'
-                        });
-                    }
-                }, {
-                    text: 'Субподрядчики',
-                    iconCls: 'subcontractors-icon',
-                    hidden: !acl.isView('subcontractors'),
-                    handler: function() {
-                        OSDN.System.Layout.getTabPanel().add({
-                            title: 'Субподрядчики',
-                            iconCls: 'subcontractors-icon',
-                            entity: 'subcontractors',
-                            xtype: 'PMS.ContragentsListAbstract',
-                            id: 'PMS.Subcontractors.List'
-                        });
-                    }
-                }]
-            }, {
-                text: 'Менеджер доступа',
-                iconCls: 'accounts_manager-icon',
-                hidden: !acl.isView('admin'),
-                handler: function() {
-                    OSDN.System.Layout.getTabPanel().add({
-                        iconCls: 'accounts_manager-icon',
-                        xtype: 'osdn.acl.layout',
-                        id: 'osdn.acl.layout'
-                    });
-                }
-            }, '-', {
-                text: 'Графики',
-                iconCls: 'prod_schd-icon',
-                menu: [{
-                    text: 'График производства',
-                    iconCls: 'prod_schd-icon',
-                    hidden: !acl.isView('admin'),
-                    handler: function() {
-                        window.open(OSDN.ABSOLUTE_PATH + '/orders/report/schedule-production');
-                    }
-                }, {
-                    text: 'График монтажа',
-                    iconCls: 'mount_schd-icon',
-                    hidden: !acl.isView('admin'),
-                    handler: function() {
-                        window.open(OSDN.ABSOLUTE_PATH + '/orders/report/schedule-mount');
-                    }
-                }, {
-                    text: 'План работ',
-                    iconCls: 'work_schd-icon',
-                    hidden: !acl.isView('admin'),
-                    handler: function() {
-                        window.open(OSDN.ABSOLUTE_PATH + '/orders/report/planning');
-                    }
-                }]
-            }, '->', '-', this.username + ' (' + this.rolename + ')', {
-                text: 'Выход',
-                iconCls: 'exit-icon',
-                handler: function() {
-                    window.location.href = '/index/logout';
-                }
-            }] 
+            items: PMS.Menu || [] 
         });
-        
+        this.menuToolbar.add({
+            text: 'Выход - <i>' + this.username + ' (' + this.rolename + ')</i>',
+            iconCls: 'exit-icon',
+            handler: function() {
+                window.location.href = '/index/logout';
+            }
+        })
+		
+		
         this.tp = new OSDN.Layout.TabPanel({
             region: 'center',
             margins: '1 1 1 1',
