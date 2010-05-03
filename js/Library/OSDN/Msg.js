76641111b6df623bc4838cff511853ed2c196799
bvh <bvh@2e978b77-0c5f-4b2b-b7d0-db7ae35eb1e0>
2010-05-03 12:02:46 +0000

    git-svn-id: file:///var/svn-repos/e-head/trunk@35 2e978b77-0c5f-4b2b-b7d0-db7ae35eb1e0

--- js/Library/OSDN/Msg.js
+++ js/Library/OSDN/Msg.js
@@ -43,29 +43,3 @@ OSDN.Msg = {
     	return t.apply({tip: tip, text: text});
     }
 };
-
-
-/*
- * 
- * @param {Object} t
- * @param {Object} s
- * example OSDN.ActionMsg.msg('Click','You clicked on "Action 1".');
- */
-OSDN.ActionMsg = {
-    createBox: function(t, s){
-        return ['<div class="msg">',
-                '<div class="x-box-tl"><div class="x-box-tr"><div class="x-box-tc"></div></div></div>',
-                '<div class="x-box-ml"><div class="x-box-mr"><div class="x-box-mc"><h3>', t, '</h3>', s, '</div></div></div>',
-                '<div class="x-box-bl"><div class="x-box-br"><div class="x-box-bc"></div></div></div>',
-                '</div>'].join('');
-    },
-	msg : function(title, format){
-            if(!msgCt){
-                var msgCt = Ext.DomHelper.insertFirst(document.body, {id:'msg-div'}, true);
-            }
-            msgCt.alignTo(document, 't-t');
-            var s = String.format.apply(String, Array.prototype.slice.call(arguments, 1));
-            var m = Ext.DomHelper.append(msgCt, {html:OSDN.ActionMsg.createBox(title, s)}, true);
-            m.slideIn('t').pause(3).ghost("t", {remove:true});
-    }
-};
