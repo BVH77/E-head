fb3734b214aa68c5b32b8861bcf0ece9069e2a17
bvh <bvh@2e978b77-0c5f-4b2b-b7d0-db7ae35eb1e0>
2010-05-10 00:06:57 +0000

    git-svn-id: file:///var/svn-repos/e-head/trunk@55 2e978b77-0c5f-4b2b-b7d0-db7ae35eb1e0

---
+++ js/Library/xlib/AclManager.js
@@ -0,0 +1,131 @@
+Ext.ns('xlib');
+
+xlib.AclManager = function() {
+    
+    var privileges = OSDN.Acl.Storage.Privileges || {};
+    var permissions = OSDN.Acl.Storage.Permissions || {};
+    var resources = new Ext.util.MixedCollection();
+    var disabled = OSDN.Acl.Storage.isSuperAdministrator();
+//    var workflowPermissions = OSDN.Acl.Storage.getWorkflowPermissions() || {};
+    
+    
+    Ext.each(OSDN.Acl.Storage.getResources(), function(i, index) {
+        resources.add(i[0], {
+            name: i[1],
+            parent: i[2] || false
+        });
+    });
+    
+    return {
+        
+        isView: function() {
+            if (disabled) {
+                return true;
+            }
+            
+            var resourceId = this.fetchResource.apply(this, arguments);
+            return this.isAllowedPrivilege(resourceId, OSDN.Acl.Storage.getPrivileges().view);
+        },
+        
+        isAdd: function() {
+            if (disabled) {
+                return true;
+            }
+            
+            var resourceId = this.fetchResource.apply(this, arguments);
+            return this.isAllowedPrivilege(resourceId, OSDN.Acl.Storage.getPrivileges().add);
+        },
+        
+        isUpdate: function() {
+            if (disabled) {
+                return true;
+            }
+            
+            var resourceId = this.fetchResource.apply(this, arguments);
+            return this.isAllowedPrivilege(resourceId, OSDN.Acl.Storage.getPrivileges().update);
+        },
+        
+        isDelete: function() {
+            if (disabled) {
+                return true;
+            }
+            
+            var resourceId = this.fetchResource.apply(this, arguments);
+            return this.isAllowedPrivilege(resourceId, OSDN.Acl.Storage.getPrivileges()['delete']);
+        },
+        
+        isAllowedPrivilege: function(resourceId, privilege) {
+            
+            var collection = OSDN.Acl.Storage.getPermissions()[resourceId];
+            if (!collection || !Ext.isArray(collection)) {
+                return false;
+            }
+
+            if (-1 == collection.indexOf(new String(privilege).toString())) {
+                return false;
+            }
+            
+            return true;
+        },
+        
+        isAllowedWorkflow: function(name, step) {
+            
+            name = String(name).toLowerCase();
+            
+            if (!Ext.isArray(workflowPermissions[name])) {
+                return false;
+            }
+            
+            return -1 != workflowPermissions[name].indexOf(String(step).toLowerCase());
+        },
+        
+        fetchResource: function(resource) {
+            
+            var resourceId = false;
+            for (var i = 0, l = arguments.length; i < l; i++) {
+                var resourceId = this.fetchByParent(arguments[i], resourceId);
+                if (false == resourceId) {
+                    break;
+                }
+            }
+            
+            if (false === resourceId) {
+                var p = [];
+                for(var i = 0, l = arguments.length; i < l; i++) {
+                    p.push(arguments[i]);
+                }
+                
+                if (OSDN.DEBUG) {
+                    Ext.Ajax.request({
+                        url: link('admin', 'acl', 'insert-resource'),
+                        params: {
+                            resource: p.join(',')
+                        },
+                        scope: this
+                    });
+                }
+            }
+            
+            return resourceId;
+        },
+        
+        fetchByParent: function(resource, parent) {
+            
+            var resource = resource.toLowerCase();
+            var resourceId = false;
+            
+            var resourceObj = resources.find(function(item, key) {
+                if (resource == item.name) {
+                    if (false == parent || item.parent == parent) {
+                        resourceId = key;
+                        return true;
+                    }
+                }
+            });
+            
+            return resourceId;
+        }
+    };
+}();
+
+acl = xlib.AclManager;
