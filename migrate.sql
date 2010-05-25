8f3b14d98a681761f1dcb709a4a1b69fdc4852b8
bvh <bvh@2e978b77-0c5f-4b2b-b7d0-db7ae35eb1e0>
2010-05-25 16:30:03 +0000

    git-svn-id: file:///var/svn-repos/e-head/trunk@170 2e978b77-0c5f-4b2b-b7d0-db7ae35eb1e0

--- migrate.sql
+++ migrate.sql
@@ -1,3 +1,9 @@
+CREATE TABLE IF NOT EXISTS `customers` (
+  `id` int(11) unsigned NOT NULL auto_increment,
+  `name` varchar(255) NOT NULL,
+  `description` text NOT NULL,
+  PRIMARY KEY  (`id`)
+) ENGINE=InnoDB  DEFAULT CHARSET=utf8 ROW_FORMAT=COMPACT AUTO_INCREMENT=11 ;
 ALTER TABLE `orders` ADD `customer_id` INT UNSIGNED NULL DEFAULT NULL AFTER `id` , ADD INDEX ( customer_id );
 ALTER TABLE `orders` ADD FOREIGN KEY ( `customer_id` ) REFERENCES `customers` (`id`);
 INSERT INTO `customers` (`name`) SELECT DISTINCT `customer` FROM `orders` WHERE `customer` IS NOT NULL AND `customer` != '';
