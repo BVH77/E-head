59e319ae0c5dd410b799556c65dee0e6607b4471
bvh <bvh@2e978b77-0c5f-4b2b-b7d0-db7ae35eb1e0>
2010-06-24 18:58:27 +0000

    git-svn-id: file:///var/svn-repos/e-head/trunk@184 2e978b77-0c5f-4b2b-b7d0-db7ae35eb1e0

--- migrate.sql
+++ migrate.sql
@@ -9,3 +9,10 @@ ALTER TABLE `orders` ADD FOREIGN KEY ( `customer_id` ) REFERENCES `customers` (`
 INSERT INTO `customers` (`name`) SELECT DISTINCT `customer` FROM `orders` WHERE `customer` IS NOT NULL AND `customer` != '';
 UPDATE `orders` SET `customer_id` = (SELECT `id` FROM `customers` WHERE `customers`.`name` = `orders`.`customer`);
 ALTER TABLE `orders` DROP `customer`; 
+
+ALTER TABLE `suppliers` ADD `subcontractor_id` INT( 11 ) UNSIGNED NOT NULL ;
+INSERT INTO `suppliers` (`subcontractor_id`, `name`, `description`) SELECT `id`, `name`, `description` FROM `subcontractors` ;
+INSERT INTO `orders_suppliers` (`success`, `date`, `order_id`, `supplier_id`) SELECT `success`, `date`, `order_id`, `suppliers`.`id` FROM `orders_subcontractors` JOIN `suppliers` ON `suppliers`.`subcontractor_id`=`orders_subcontractors`.`subcontractor_id` ;
+ALTER TABLE `suppliers` DROP `subcontractor_id` ;
+DROP TABLE `orders_subcontractors` ;
+DROP TABLE `subcontractors` ;
