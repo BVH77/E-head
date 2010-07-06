531669b33f19bd56645119fac793a4e1a455d227
bvh <bvh@2e978b77-0c5f-4b2b-b7d0-db7ae35eb1e0>
2010-07-06 20:53:43 +0000

    git-svn-id: file:///var/svn-repos/e-head/trunk@195 2e978b77-0c5f-4b2b-b7d0-db7ae35eb1e0

---
+++ migrate_07-07-2010.sql
@@ -0,0 +1,37 @@
+CREATE TABLE IF NOT EXISTS `customers` (
+  `id` int(11) unsigned NOT NULL auto_increment,
+  `name` varchar(255) NOT NULL,
+  `description` text NOT NULL,
+  PRIMARY KEY  (`id`)
+) ENGINE=InnoDB  DEFAULT CHARSET=utf8 ROW_FORMAT=COMPACT AUTO_INCREMENT=11 ;
+ALTER TABLE `orders` ADD `customer_id` INT UNSIGNED NULL DEFAULT NULL AFTER `id` , ADD INDEX ( customer_id );
+ALTER TABLE `orders` ADD FOREIGN KEY ( `customer_id` ) REFERENCES `customers` (`id`);
+
+-- ------------------------------ updated on quarant ---------------------------
+
+INSERT INTO `customers` (`name`) SELECT DISTINCT `customer` FROM `orders` WHERE `customer` IS NOT NULL AND `customer` != '';
+UPDATE `orders` SET `customer_id` = (SELECT `id` FROM `customers` WHERE `customers`.`name` = `orders`.`customer`);
+ALTER TABLE `orders` DROP `customer`; 
+
+ALTER TABLE `suppliers` ADD `subcontractor_id` INT( 11 ) UNSIGNED NOT NULL ;
+INSERT INTO `suppliers` (`subcontractor_id`, `name`, `description`) SELECT `id`, `name`, `description` FROM `subcontractors` ;
+INSERT INTO `orders_suppliers` (`success`, `date`, `order_id`, `supplier_id`) SELECT `success`, `date`, `order_id`, `suppliers`.`id` FROM `orders_subcontractors` JOIN `suppliers` ON `suppliers`.`subcontractor_id`=`orders_subcontractors`.`subcontractor_id` ;
+ALTER TABLE `suppliers` DROP `subcontractor_id` ;
+DROP TABLE `orders_subcontractors` ;
+DROP TABLE `subcontractors` ;
+
+ALTER TABLE `orders_suppliers` ADD `cost` INT NULL ;
+ALTER TABLE `orders_suppliers` ADD `note` TEXT NULL ;
+ALTER TABLE `orders_suppliers` DROP PRIMARY KEY ;
+ALTER TABLE `orders_suppliers` DROP FOREIGN KEY `orders_suppliers_ibfk_1`  ;
+ALTER TABLE `orders_suppliers` DROP FOREIGN KEY `orders_suppliers_ibfk_2` ;
+ALTER TABLE `orders_suppliers` ADD `id` INT( 11 ) UNSIGNED NOT NULL AUTO_INCREMENT PRIMARY KEY FIRST ;
+ALTER TABLE `orders_suppliers` ADD UNIQUE ( `order_id`, `supplier_id` );
+ALTER TABLE `orders_suppliers` ADD FOREIGN KEY ( `order_id` ) REFERENCES `orders` (`id`) ON DELETE CASCADE ;
+ALTER TABLE `orders_suppliers` ADD FOREIGN KEY ( `supplier_id` ) REFERENCES `suppliers` (`id`) ON DELETE CASCADE ;
+
+ALTER TABLE `orders` ADD `archive_date` TIMESTAMP NULL ;
+ALTER TABLE `orders` ADD `invoice_number` VARCHAR( 255 ) NULL ;
+ALTER TABLE `orders` ADD `invoice_date` DATE NULL ;
+ALTER TABLE `orders` ADD `act_number` VARCHAR( 255 ) NULL ;
+ALTER TABLE `orders` ADD `act_date` DATE NULL ;
