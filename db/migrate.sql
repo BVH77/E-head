f57a0a6254a0d369876bf9a6d5c5d4fa85acf80c
bvh <bvh@2e978b77-0c5f-4b2b-b7d0-db7ae35eb1e0>
2010-08-20 16:46:51 +0000

    git-svn-id: file:///var/svn-repos/e-head/trunk@256 2e978b77-0c5f-4b2b-b7d0-db7ae35eb1e0

--- db/migrate.sql
+++
@@ -1,3 +0,0 @@
-UPDATE `acl_resources` SET `title` = 'Скрывать заказы без производства' WHERE `name` = 'hideproduction';
-UPDATE `acl_resources` SET `title` = 'Скрывать заказы без монтажа' WHERE `name` = 'hidemount';
-UPDATE `acl_resources` SET `title` = 'Скрывать не свои заказы' WHERE `name` = 'owncheck';
