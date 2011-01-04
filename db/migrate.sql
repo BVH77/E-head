041e4eb2d69a1ee9a182f68cb2ad25b2d53b0d98
bvh <bvh@2e978b77-0c5f-4b2b-b7d0-db7ae35eb1e0>
2011-01-04 00:09:50 +0000

    git-svn-id: file:///var/svn-repos/e-head/trunk@346 2e978b77-0c5f-4b2b-b7d0-db7ae35eb1e0

---
+++ db/migrate.sql
@@ -0,0 +1,13 @@
+ALTER TABLE `storage_assets` ADD `qty` INT( 11 ) UNSIGNED NOT NULL;
+ALTER TABLE `storage_assets` ADD `unit_price` INT( 11 ) UNSIGNED NOT NULL;
+UPDATE `storage_assets` SET `qty` = (SELECT `qty` FROM `storage_availability` WHERE `asset_id`=`storage_assets`.`id` LIMIT 1);
+DROP TABLE `storage_availability`;
+
+CREATE TABLE `storage_measures` (
+`name` VARCHAR( 50 ) NOT NULL ,
+PRIMARY KEY ( `name` )
+) ENGINE = InnoDB;
+
+INSERT INTO `storage_measures` (`name`) VALUES ('шт.'), ('л'), ('кг'), ('м'), ('кв.м'), ('куб.м');
+
+-- Migrated on pms_quarant
