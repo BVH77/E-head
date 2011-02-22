a44e8cd12e1e64ba957adee4adfd16e9c96ba9bd
bvh <bvh@2e978b77-0c5f-4b2b-b7d0-db7ae35eb1e0>
2011-02-22 20:23:32 +0000

    git-svn-id: file:///var/svn-repos/e-head/trunk@394 2e978b77-0c5f-4b2b-b7d0-db7ae35eb1e0

---
+++ db/migrate_2011-02-21.sql
@@ -0,0 +1,17 @@
+CREATE TABLE `storage_assets_categories` (
+`id` INT UNSIGNED NOT NULL AUTO_INCREMENT PRIMARY KEY ,
+`asset_id` INT UNSIGNED NOT NULL ,
+`category_id` INT UNSIGNED NOT NULL ,
+INDEX ( `asset_id` ), 
+INDEX (`category_id` ),
+UNIQUE `pair` ( `asset_id` , `category_id` )
+) ENGINE = InnoDB;
+ALTER TABLE `storage_assets_categories` ADD UNIQUE `pair` ( `asset_id` , `category_id` );
+ALTER TABLE `storage_assets_categories` ADD FOREIGN KEY ( `asset_id` ) REFERENCES `storage_assets` (`id`) ON DELETE CASCADE ;
+ALTER TABLE `storage_assets_categories` ADD FOREIGN KEY ( `category_id` ) REFERENCES `storage_categories` (`id`) ON DELETE CASCADE ;
+
+INSERT INTO `storage_assets_categories` (`asset_id`, `category_id`) SELECT `id`, `category_id` FROM `storage_assets` WHERE `category_id` IS NOT NULL;
+
+ALTER TABLE `storage_assets` ADD `checked` TINYINT( 1 ) NOT NULL ;
+
+ALTER TABLE `storage_assets` DROP `category_id`;
