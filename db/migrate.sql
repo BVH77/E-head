30561869f87c0c7c3bb18060b1ea492a90c11ed6
bvh <bvh@2e978b77-0c5f-4b2b-b7d0-db7ae35eb1e0>
2011-03-11 13:32:26 +0000

    git-svn-id: file:///var/svn-repos/e-head/trunk@408 2e978b77-0c5f-4b2b-b7d0-db7ae35eb1e0

--- db/migrate.sql
+++ db/migrate.sql
@@ -14,4 +14,8 @@ INDEX ( `account_id` )
 
 ALTER TABLE `notice` ADD FOREIGN KEY ( `account_id` ) REFERENCES `accounts` (`id`) ON DELETE RESTRICT ;
 
--- Updated on quarant
+-- Updated on quarant, kovshilov, kosorilov
+
+ALTER TABLE `notice` DROP `title`;
+ALTER TABLE `notice` ADD `type` ENUM( 'объявление', 'уведомление', 'приказ' ) NOT NULL AFTER `id`;
+ALTER TABLE `notice` ADD `dst_type` ENUM( 'all', 'roles', 'users' ) NOT NULL AFTER `type`;
