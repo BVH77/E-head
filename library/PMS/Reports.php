83ee6a0245dcec2c6f3a7e640a148c37672897b5
bvh <bvh@2e978b77-0c5f-4b2b-b7d0-db7ae35eb1e0>
2011-04-11 09:55:05 +0000

    git-svn-id: file:///var/svn-repos/e-head/trunk@439 2e978b77-0c5f-4b2b-b7d0-db7ae35eb1e0

--- library/PMS/Reports.php
+++ library/PMS/Reports.php
@@ -145,64 +145,6 @@ class PMS_Reports
 
     }
 
-    public function generateStaff(array $params)
-    {
-        $response = new OSDN_Response();
-
-        $f = new OSDN_Filter_Input(array(
-            '*' => 'StringTrim'
-        ), array(
-            'start'  => array('Date', 'allowEmpty' => true),
-            'end'    => array('Date', 'allowEmpty' => true)
-        ), $params);
-
-        $response->addInputStatus($f);
-        if ($response->hasNotSuccess()) {
-            return $response;
-        }
-
-        $tableHr = new PMS_Staff_Table();
-        $tableStaff = new PMS_Staff_Hr_Table();
-
-        /*
-        $select = $this->_tableOrders->getAdapter()->select()
-        ->from(array('o' => $this->_tableOrders->getTableName()),
-            array(
-                'summ_total'    => new Zend_Db_Expr('SUM(`cost`)'),
-                'summ_success'  => new Zend_Db_Expr('SUM(
-                    IF(`success_date_fact` IS NOT NULL,`cost`,0))'),
-                'failed_orders_count'  => new Zend_Db_Expr('SUM(
-                    IF(`success_date_planned` < NOW()
-                        AND `success_date_fact` IS NOT NULL,
-                    0,1))')
-            )
-        )
-        ->joinLeft(array('a' => $this->_tableAccounts->getTableName()),
-            'o.creator_id=a.id', 'name')
-        ->group('creator_id')
-        ;
-        if (!empty($f->start)) {
-            $select->where('created >= ?', $f->start);
-        }
-        if (!empty($f->end)) {
-            $select->where('created <= ?', $f->end);
-        }
-        try {
-            $rows = $select->query()->fetchAll();
-        } catch (Exception $e) {
-            //throw $e;
-            return $response->addStatus(new PMS_Status(PMS_Status::DATABASE_ERROR));
-        }
-        $result = array(
-            'rows'  => $rows,
-            'start' => $f->start,
-            'end'   => $f->end
-        );
-        $response->data = $result;
-        */
-        return $response->addStatus(new PMS_Status(PMS_Status::OK));
-    }
-
     //--------------------------------------------------------------------------
 
     private function getMonthDays($date)
