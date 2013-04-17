fdb68057085dde3f2f9565f946225d50c197997d
bvh <bvh@2e978b77-0c5f-4b2b-b7d0-db7ae35eb1e0>
2013-04-17 18:18:31 +0000

    git-svn-id: file:///var/svn-repos/e-head/trunk@717 2e978b77-0c5f-4b2b-b7d0-db7ae35eb1e0

--- js/PMS/Organizer/Manager.js
+++ js/PMS/Organizer/Manager.js
@@ -6,38 +6,59 @@ PMS.Organizer.Manager = Ext.extend(Ext.Component, {
     
     initComponent: function() {
         
-        this.getActiveTasksCount();
+        this.getActiveTasksCount(function(count) {
+            
+            if (count > 0) {
+                this.getWindow(new PMS.Organizer.List({
+                    title: false
+                })).show();
+            }
+            
+            this.updateButton(count);
+        });
         
         PMS.Organizer.Manager.superclass.initComponent.apply(this, arguments);
+        
+        
+        // Periodic check tasks & update button  
+        Ext.TaskMgr.start({
+            run: function() {
+                this.getActiveTasksCount(this.updateButton);
+            },
+            scope: this,
+            interval: 60000 // = 1 munute
+        });
     },
     
-    getActiveTasksCount: function() {
+    getActiveTasksCount: function(f) {
     	
         Ext.Ajax.request({
             url: link('organizer', 'index', 'get-active-tasks-count'),
-            callback: function (options, success, response) {
+            callback: function(options, success, response) {
                 var res = xlib.decode(response.responseText);
-                if (true == success && res && true == res.success && res.count > 0) {
-                    this.getWindow(new PMS.Organizer.List({
-                        title: false
-                    }), res.count).show();
-                }
-                var b = Ext.getCmp('Organizer-Button');
-                if (b && b.setText) {
-                    b.setText(b.getText() 
-                    + ' <font color="red"><b>' + (res.count || 0) + '</b></font>'); 
+                if (true == success && res && true == res.success) {
+                    f.call(this, res.count);
                 }
             },
             scope: this
         });
     },
     
-    // Private functions 
+    updateButton: function(count) {
+        
+        var b = Ext.getCmp('Organizer-Button');
+        
+        if (b && b.setText) {
+            var text = (b.srctext || 'Задачи') 
+                     + ' <font color="red"><b>' + (count || 0) + '</b></font>';
+            b.setText(text); 
+        }
+    },
     
-    getWindow: function(content, count) {
+    getWindow: function(content) {
          
         var w = new Ext.Window({
-            title: 'Мои задачи <font color="red"><b>' + (count || 0) + '</b></font>',
+            title: 'Мои задачи',
             border: false,
             width: 700,
             height: 500,
