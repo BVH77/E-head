145ee3a1aeaf326c99d8dcdf2290d573cf7ff26c
bvh <bvh@2e978b77-0c5f-4b2b-b7d0-db7ae35eb1e0>
2013-05-22 17:31:51 +0000

    git-svn-id: file:///var/svn-repos/e-head/trunk@725 2e978b77-0c5f-4b2b-b7d0-db7ae35eb1e0

--- db/pms_demo_clean.sql
+++ db/pms_demo_clean.sql
@@ -842,6 +842,19 @@ CREATE TABLE IF NOT EXISTS `orders_budget` (
   KEY `order_id` (`order_id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 AUTO_INCREMENT=1 ;
 
+DROP TABLE IF EXISTS `organizer`;
+CREATE TABLE IF NOT EXISTS `organizer` (
+  `id` int(11) unsigned NOT NULL AUTO_INCREMENT,
+  `ondate` date NOT NULL,
+  `ontime` time NOT NULL,
+  `text` text NOT NULL,
+  `account_id` int(11) unsigned NOT NULL,
+  `created` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  `closed` timestamp NULL DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  KEY `account_id` (`account_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 AUTO_INCREMENT=1
+
 -- ***
 
 -- 
@@ -924,4 +937,7 @@ ALTER TABLE `orders_payments`
 TER TABLE `orders_budget`
   ADD CONSTRAINT `orders_budget_ibfk_1` FOREIGN KEY (`order_id`) REFERENCES `orders` (`id`) ON DELETE CASCADE ON UPDATE CASCADE;  
 
+ ALTER TABLE `organizer`
+  ADD CONSTRAINT `organizer_ibfk_1` FOREIGN KEY (`account_id`) REFERENCES `accounts` (`id`);
+  
 SET FOREIGN_KEY_CHECKS=1;
