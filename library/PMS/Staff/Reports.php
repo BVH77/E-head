a59539f76af71373823ccef77e9af81546297a82
bvh <bvh@2e978b77-0c5f-4b2b-b7d0-db7ae35eb1e0>
2013-11-01 15:47:57 +0000

    git-svn-id: file:///var/svn-repos/e-head/trunk@801 2e978b77-0c5f-4b2b-b7d0-db7ae35eb1e0

--- library/PMS/Staff/Reports.php
+++ library/PMS/Staff/Reports.php
@@ -314,6 +314,7 @@ class PMS_Staff_Reports
         }
 
         $response->data = $rows;
+        $response->debug = $select->assemble();
         return $response->addStatus(new PMS_Status(PMS_Status::OK));
     }
 }
